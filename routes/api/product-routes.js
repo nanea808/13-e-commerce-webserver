0b00604aef8189186eb750e139ccdad25f809368
Ethan Willers <willers.enw@gmail.com>
2023-02-04 23:19:18 -0800

    product routes added

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,33 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const dbProducts = await Product.findAll({
+      include: [{ model: Category}],
+      attributes: { exclude: ['category_id'] }
+    });
+    res.status(200).json(dbProducts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const dbProduct = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }],
+      attributes: { exclude: ['category_id'] }
+    });
+    res.status(200).json(dbProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  } 
 });
 
 // create new product
